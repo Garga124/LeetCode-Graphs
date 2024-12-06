d5dfc8423a711f189752046258c5380430ee895c
Ruhi Garga <gargaruhi9@gmail.com>
2024-12-06 19:02:47 +0000

    Update 802-Find_EventualSafeStates.cs

--- Medium/802-Find_EventualSafeStates.cs
+++ Medium/802-Find_EventualSafeStates.cs
@@ -1,103 +1,89 @@
-/*
-Time complexity:O(V + E), the Time Complexity of this method is the same as the time complexity of DFS traversal which is O(V+E).
+/*Time complexity:O(V + E), the Time Complexity of this method is the same as the time complexity of DFS traversal which is O(V+E).
 Space complexity:O(V). The extra space of V is needed for the stack, recursionstack and visited array
-
 */
-
 public class Solution {
-    public int[] FindOrder(int numCourses, int[][] prerequisites) {
-        Dictionary<int,List<int>> adjList = BuildAdjacencyList(prerequisites,numCourses);
-        return TopologicalSort(numCourses,adjList);
+    public IList<int> EventualSafeNodes(int[][] graph) {
+        //Create an Adjacency List
+        Dictionary<int, List<int>> adjList = BuildAdjacencyList(graph);
+        //Get the number of nodes 
+        int vertex = graph.GetLength(0);
+        //Call method to return the safenodes
+        return EventualSafeNodes(adjList,vertex);
+         
+    }
+
+    //Method to calculate the safe nodes
+    public IList<int> EventualSafeNodes(Dictionary<int,List<int>> adjList,int vertex)
+    {
+        //Define the list of the variables required
+        bool[] visited = new bool[vertex];
+        bool[] recTraversal = new bool[vertex];
+        bool[] check = new bool[vertex];
+        IList<int> result = new List<int>();
+
+        //Loop on all the nodes to traverse using DFS traversal and identify if there is a cycle
+        for(int i =0;i<vertex;i++)
+        {
+            if(!visited[i])
+            {
+                 dfsTraversal(i,adjList,visited,recTraversal,check);
+            }
+        }
+
+        //Non-cyclic nodes will be marked as true . Print those nodes
+        for(int i =0;i<vertex;i++)
+        {
+            if(check[i])
+            {
+                result.Add(i);
+
+            }
+        }
+        return result;
 
     }
 
-    public int[] TopologicalSort(int numCourses, Dictionary<int,List<int>> adjList)
-    {
-        int[] orderOfCourses = new int[numCourses];
-        //Create a Stack to store the order
-        Stack<int> stack = new Stack<int>();
-        //Create a visited array
-        bool[] visited = new bool[numCourses];
-        //Create a recursion stack to verify if the path has been visited or not
-        bool[] recStack = new bool[numCourses];
-        bool isCyclic = false;
-   
-       //Call the recursive helper function to detect cycle in the graph
-        for(int i = 0;i<numCourses;i++)
-        {
-            if(!visited[i] && DFSSort(i,adjList,visited,stack,recStack))
-            { 
-                isCyclic = true;
-            }
-           
-        }
-       
-       // If there isnt a cycle then return the sorted list
-        if(!isCyclic)
-        {
-            int lengthOfStack = stack.Count;
-            for(int i =0;i<lengthOfStack;i++)
-            {
-            orderOfCourses[i] = stack.Pop();
-            }
-            return orderOfCourses;
-        } 
-        
-        return new int[]{};
-     
+    //DFS traversal of the graph
 
-    }
-    
-    public bool DFSSort(int vertex,Dictionary<int,List<int>> adjList, bool[] visited, Stack<int> stack,bool[] recStack)
+    public bool dfsTraversal(int vertex, Dictionary<int,List<int>> adjList, bool[] visited, bool[] recTraversal, bool[]check)
     {
-       if (!visited[vertex]) 
-       {
-        visited[vertex] = true;
-        recStack[vertex]= true;
-        //find all the adjacent edges
-        foreach(int edge in adjList[vertex])
+        if(!visited[vertex])
         {
-           //if its already visited then do not need to traverse  
-            if(!visited[edge] && DFSSort(edge,adjList,visited,stack,recStack))
+            check[vertex] = false;
+            visited[vertex] = true;
+            recTraversal[vertex] = true;
+            foreach(int edge in adjList[vertex])
             {
-                 return true;
-            } 
-            else if(recStack[edge])
-            {
-                //There is a cycle
-                return true;
+                if(!visited[edge] && dfsTraversal(edge,adjList,visited,recTraversal,check )){
+                    return true;
+                }else if(recTraversal[edge])
+                {
+                    return true;
+                }
             }
         }
-       }
-    
-        //Set the recStack false when we are going back to the
-        // Remove the vertex from recursion stack
-        recStack[vertex] = false;
-        //Push it in stack
-        stack.Push(vertex);
+        check[vertex] = true;
+        recTraversal[vertex] = false;
         return false;
-       
     }
 
-    public Dictionary<int,List<int>> BuildAdjacencyList(int[][] prerequisites, int vertices)
+    public Dictionary<int, List<int>> BuildAdjacencyList(int[][] graph)
     {
-        Dictionary<int,List<int>> adjList = new Dictionary<int,List<int>>();
-            for(int i=0;i<vertices;i++)
-            {
-                adjList[i] = new List<int>();
-            }
-
-            foreach(int[] edge in prerequisites)
-            {
-                //edge from v to u
-            int u = edge[0];    
-            int v = edge[1];
-            adjList[v].Add(u);
-            
-            }
+        Dictionary<int, List<int>> adjList = new Dictionary<int, List<int>>();
+        int vertex = graph.GetLength(0);
+        for(int i=0;i<vertex;i++)
+        {
+            adjList[i] = new List<int>();
+        }
+        int count = 0;
+        foreach(int[] edge in graph)
+        {
+           foreach(int item in edge)
+           {
+            adjList[count].Add(item);
+           }
+           count++;
+        }
         return adjList;
-
-
     }
-
 }
